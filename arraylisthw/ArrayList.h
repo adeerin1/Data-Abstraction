47801e5841e7d989dbee47e8db25b0577eebd638
Andrew Deering <adeerin1@trinity.edu>
2022-09-21 12:23:04 -0500

    after class wednesday

--- arraylisthw/ArrayList.h
+++ arraylisthw/ArrayList.h
@@ -29,28 +29,61 @@ public:
 	class const_iterator {
                 public:
 			T* loc;
-                	const_iterator(T *l):loc(l) {};
-                	const_iterator() {};
-                	const_iterator(const const_iterator &i) {};
-                	T &operator*() { return *loc; };
+                	const_iterator(T *l):loc(l) { loc = l;}; //parameterized constructor
+                	const_iterator() { loc == nullptr; }; //default constructor
+                	const_iterator(const const_iterator &i) {}; //copy constructor
+                	const T &operator*() { return *loc; };
                 	bool operator==(const const_iterator &i) const {return loc == i.loc;};
                 	bool operator!=(const const_iterator &i) const { return !(*this == i);};
-                	const_iterator &operator=(const const_iterator &i) {};
+                	const_iterator &operator=(const const_iterator &i) {loc = i.loc; return *this;};
                 	const_iterator &operator++() {++loc; return*this; };
-                	const_iterator &operator--() {};
-                	const_iterator operator++(int) {};
-                	const_iterator operator--(int) {};
+                	const_iterator &operator--() {--loc; return *this; };
+                	const_iterator operator++(int) {
+				const_iterator tmp(*this);
+				++ loc;
+				return tmp;
+			};
+                	const_iterator operator--(int) {
+				const_iterator tmp(*this);
+				--loc;
+				return tmp;
+			};
         };
 
 // General Methods
 ArrayList():data{new T[max_sz]}, ct{0}, max_sz{10} {}; //default constructor
-ArrayList(const ArrayList &that) {}; //copy constructor
-ArrayList<T> &operator=(const ArrayList<T> &al) {}; //copy assignment
+ArrayList(const ArrayList &that):data{new T[that.max_sz]}, ct{that.ct}, max_sz{that.max_sz} {
+	for(int i = 0; i < ct; ++i) {
+		data[i] = that.data[i];
+	}
+	return *this;
+}; //copy constructor
+ArrayList<T> &operator=(const ArrayList<T> &al) {
+	ct = al.ct;
+	max_sz = al.max_sz;
+	delete [] data;
+	data = new T[max_sz];
+	for(int i = 0; i < ct; ++i) {
+		data[i] = al.data[i];
+	}
+	return *this;
+}; //copy assignment
 
 ~ArrayList() {
 	delete [] data;
 }; //destructor
-void push_back(const T &t) {}; // add to the end.
+void push_back(const T &t) {
+	if (ct == max_sz) { //allocating more array length
+		T* tmp = new T[max_sz*2];
+		for(int i = 0; i < ct; ++i) {
+			tmp[i] = data[i];
+		}
+		delete [] data;
+		data = tmp;
+		max_sz *= 2;
+	}
+	data[++ct] = t;
+}; // add to the end.
 void pop_back() {}; // remove last element.
 int size() const {return ct;}; //return the list size as an int
 void clear() {ct = 0;}; //remove all elements from the arraylist
@@ -59,8 +92,7 @@ void insert(const T &t, int index) {
                 data[i] = data[i-1];
         }
         data[index] = t;
-++ct;
-
+	++ct;
 }
 
 const T &operator[](int index) const {return data[index];}; // get the element at index.
